ed3128fb946d22d6e54f85692b8056b08e6cbe17
Grace Jia <gracejia25@gmail.com>
2025-06-18 16:37:57 -0400

    Add basic test script

---
+++ scripts/run_basic_test.sh
@@ -0,0 +1,3 @@
+python3 attacks/fit/eval.py --app llm_nitro --vocab-data data/llm/all.csv --num-test-samples 1
+python3 attacks/USENIX22-ihop-code/page_experiment.py --app llm --num-train-samples 10 --num-test-samples 1
+python3 attacks/naive_bayes/train_eval.py --app llm --num-train-samples 10 --num-test-samples 1
